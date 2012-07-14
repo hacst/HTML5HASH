936b8c3b5313c09223361ea4e0cbcc0de7fedd65
Stefan Hacker <dd0t@users.sourceforge.net>
2012-07-14 11:59:04 +0200

    Remove debugging stubs

--- html5hash.js
+++ html5hash.js
@@ -2,7 +2,7 @@ $().ready(function () {
 
 
     /*
-     * Helpers one wouldn't if JS didn't suck...
+     * Helpers one wouldn't need if JS didn't suck...
      */
 
 
@@ -212,44 +212,9 @@ $().ready(function () {
         evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
     }
 
-    function debuggingstubstuff() {
-        // Dummy testing content
-        var uid = "filehash" + getUnique();
-
-        $("#list").append('<li id="' + uid + '">'
-                    + '<b>' + 'Just testin.html' + ' <span class="progresstext"></span></b>'
-                    + '<div class="progress"></div>'
-                    + '</li>');
-
-        $("#" + uid + " .progress").progressbar({ value: 20 });
-
-        // Dummy testing content
-        var uid = "filehash" + getUnique();
-
-        $("#list").append('<li id="' + uid + '">'
-                    + '<b>' + 'Just testin2.html' + ' <span class="progresstext"></span></b>'
-                    + '<div class="progress"></div>'
-                    + '</li>');
-
-        $("#" + uid + " .progress").progressbar({ value: 50 });
-
-        // Dummy testing content
-        var uid = "filehash" + getUnique();
-
-        $("#list").append('<li id="' + uid + '">'
-                    + '<b>' + 'Just testin3.html' + ' <span class="progresstext"></span></b>'
-                    + '<div class="progress"></div>'
-                    + '</li>');
-
-        $("#" + uid + " .progress").progressbar({ value: 90 });
-    };
-
-
     // Setup the dnd listeners.
     var dropZone = document.getElementById('drop_zone');
     dropZone.addEventListener('dragover', handleDragOver, false);
     dropZone.addEventListener('drop', handleFileSelect, false);
-
-    //debuggingstubstuff();
 
 });
