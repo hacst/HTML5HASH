48dc22cffaa08198e1d633f1d099ef8a3246d02e
Stefan Hacker <dd0t@users.sourceforge.net>
2012-07-15 04:19:10 +0200

    disable G+ until we actually have a domain

--- html5hash.js
+++ html5hash.js
@@ -236,11 +236,11 @@ $().ready(function () {
         return true;
     }
 
-    (function () {
+    /*(function () {
         var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
         po.src = 'https://apis.google.com/js/plusone.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-    })();
+    })();*/
 
     if (!compatible()) {
         return; // Nevermind initialising the handlers
